2faeb485eb23bc90a2bd1df00a1289190598d6d4
Yanlong Wang <yanlong.wang@naiver.org>
2025-06-05 18:44:02 +0800

    wip

--- src/reactive-element.ts
+++ src/reactive-element.ts
@@ -1,8 +1,16 @@
+import { REACTIVE_TEMPLATE_CSS, REACTIVE_TEMPLATE_HTML } from "./decorators/template";
 import { activateReactivity, ReactivityHost } from "./decorators/reactive";
 
 export interface ReactiveElement extends ReactivityHost { }
 
 export class ReactiveElement extends HTMLElement {
+    static [REACTIVE_TEMPLATE_CSS]: string = '';
+    static [REACTIVE_TEMPLATE_HTML]: string = '';
+
+    protected customElement: Record<string, Element> = {
+        
+    };
+
     constructor() {
         super();
         activateReactivity(this);
